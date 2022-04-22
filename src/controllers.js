6656d9535191a99910cc7c7e76cc9a11e6104783
Franco <franco@DESKTOP-2DLHK1B.localdomain>
2022-04-22 12:30:07 -0300

    -Agrago la funcion de 'Resta'

--- src/controllers.js
+++ src/controllers.js
@@ -16,7 +16,14 @@ router.get("/add/:a/:b", async function (req, res) {
 });
 
 router.get("/res/:a/:b", async function (req, res) {
-    return res.send({ result: "No implementado" });
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+    const result = a - b;
+
+    await insertOperation(a,b,"LESS",result);
+
+    return res.send({ result });
 });
 
 router.get("/mul/:a/:b", async function (req, res) {
